1d4c3f6701a0dd0e53da2a9b0f467949fd68314d
davilla1993 <carlogbossou93@gmail.com>
2022-07-26 23:28:07 +0000

    26/07/2022

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -58,10 +58,10 @@ export class CheckoutComponent implements OnInit {
       }),
 
       creditCard: this.formBuilder.group({
-        cardType: ['', Validators.required],
+        cardType: new FormControl('', [Validators.required]),
         nameOnCard: new FormControl('', [Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhitespace]),
-        cardNumber: new FormControl(['', Validators.required, Validators.pattern('[0-9]{16}')]),
-        securityCode: new FormControl(['', Validators.required, Validators.pattern('[0-9]{3}')]),
+        cardNumber: new FormControl('', [Validators.required, Validators.pattern('[0-9]{16}')]),
+        securityCode: new FormControl('', [Validators.required, Validators.pattern('[0-9]{3}')]),
         expirationMonth: [''],
         expirationYear: ['']
 
