3c2e73d02146b36aeb63fb6fd4d28c82d70d61b0
davilla1993 <carlogbossou93@gmail.com>
2022-08-03 21:28:00 +0000

    03/08/2022

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa5ywggc6jlXk6ZA5d7',
         issuer: 'https://dev-92663353.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -3,6 +3,7 @@ import { Inject, Injectable } from '@angular/core';
 import { OktaAuth } from '@okta/okta-auth-js'; 
 import { OktaAuthStateService, OKTA_AUTH } from '@okta/okta-angular';
 import { from, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +21,7 @@ export class AuthInterceptorService implements HttpInterceptor {
   private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
 
     // Only add an access token for secured endpoints
+    const theEndpoint = environment.luv2shopApiUrl + '/orders';
     const securedEndpoints = ['http://localhost:8080/api/orders'];
 
     if(securedEndpoints.some(url => request.urlWithParams.includes(url))) {
